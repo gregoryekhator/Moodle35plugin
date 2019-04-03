75817c467107bde383141c285f611c7796a973b7
gregoryekhator <41048560+gregoryekhator@users.noreply.github.com>
2019-04-03 18:36:00 +0100

    Add files via upload
    Moodle 3.5 plugin for courses AI

---
+++ ai_courses2/backup/moodle2/backup_ai_courses2_stepslib.php
@@ -0,0 +1,60 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Backup steps for block_ai_courses2 are defined here.
+ *
+ * @package     block_ai_courses2
+ * @category    backup
+ * @copyright   2018 Debonair Training <greg@debonairtraining.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// For more information about the backup and restore process, please visit:
+// https://docs.moodle.org/dev/Backup_2.0_for_developers
+// https://docs.moodle.org/dev/Restore_2.0_for_developers
+
+/**
+ * Define the complete structure for backup, with file and id annotations.
+ */
+class backup_ai_courses2_block_structure_step extends backup_block_structure_step {
+
+    /**
+     * Defines the structure of the resulting xml file.
+     *
+     * @return backup_nested_element The structure wrapped in the block tag.
+     */
+    protected function define_structure() {
+        global $DB;
+
+        // Replace with the attributes and final elements that the element will handle.
+        $attributes = null;
+        $final_elements = null;
+        $root = new backup_nested_element('block_ai_courses2', $attributes, $final_elements);
+
+        // Build the tree with these elements with $root as the root of the backup tree.
+
+        // Define the source tables for the elements.
+
+        // Define id annotations.
+
+        // Define file annotations.
+
+        return $this->prepare_block_structure($root);
+    }
+}
